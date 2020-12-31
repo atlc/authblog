a3e70c5b830b666ce59bbb6121a9db762a02ff8c
Andrew Cartwright <andrewlloydcartwright+atlc@gmail.com>
2020-12-31 02:16:51 -0600

    Integrated the HIBP helper into register router on the backend. Need to fix API helper to allow returning not-ok res values so that I can return error messages with meaningful data from the backend

--- src/server/routes/auth/register.ts
+++ src/server/routes/auth/register.ts
@@ -1,5 +1,6 @@
 import * as express from 'express';
-import { HashPassword } from '../../utils/security/passwords'
+import { HashPassword } from '../../utils/security/passwords';
+import { validate } from '../../utils/security/have-i-been-pwned';
 import DB from '../../db';
 
 const router = express.Router();
@@ -15,6 +16,13 @@ router.post('/', async (req, res, next) => {
         const isNewUser = await DB.Users.get.user_by_email(email);
         if (!!(isNewUser[0])) return res.status(403).send(`An account using the email address ${email} already exists.`);
         
+        // validate function returns the number of matches if it's a bad password, otherwise if it's unique/unbreached it returns false
+        const isBadPassword = !!validate(password);
+        
+        if (isBadPassword) {
+            res.status(403).send(`Bad password! That has been found in ${isBadPassword} account breaches!`);
+        }
+
         const hash = HashPassword(password);
         const newUser = await DB.Users.do.create(firstname, lastname, email, hash, roles, avatar);
         if (newUser.insertId) {
--- src/server/utils/security/have-i-been-pwned.ts
+++ src/server/utils/security/have-i-been-pwned.ts
@@ -1,15 +1,15 @@
+import { match } from 'assert';
 import * as crypto from 'crypto';
 import * as rp from 'request-promise';
 
-const validate = async (password) => {
-
-    // Exposed plaintext passwords are hashed as SHA-1 per the maintainer of HIBP
+export const validate = async (password) => {
+    // Breached plaintext passwords are hashed as SHA-1 per the maintainer of HIBP
     const shasum = crypto.createHash('sha1');
     shasum.update(password);
     const hashedPass = shasum.digest('hex');
 
     // Pass the API the first 5 characters of the resulting hash, and it returns a list of 
-    // ALL hashes that begin with those 5 characters, with those characters truncated
+    // ALL hashes that begin with those 5 characters, with those 5 characters popped off
     const matchesList = await rp(`https://api.pwnedpasswords.com/range/${hashedPass.slice(0, 5)}`);
     const matchesArray = matchesList.split('\r\n');
     const hashPrefixMatchString = hashedPass.slice(5, hashedPass.length).toUpperCase();
@@ -17,16 +17,8 @@ const validate = async (password) => {
 
     if (matchIndex !== -1) {
         const matchInstances = Number(matchesArray[matchIndex].split(':')[1]).toLocaleString();
-        console.log(`Bad password! ${password} has been found in ${matchInstances} separate account breaches!`)
+        return matchInstances;
     } else {
-        console.log(`Good password! ${password} has NOT been found in any breaches :)`)
+        return false;
     }
 }
-
-validate('password123');
-validate('qwerty');
-validate('123456');
-validate('123456789');
-validate('$y7IMxkoE$kibQ@4FUK!AD3Znp87g5@PjkSSGoPF6J3NfG0KVFra$QWF804axfWIUBtps@g6$3&xYa');
-validate('0zfzJ6fOB1%dG1tNbBgn!^fb0r7&VgFTTb%fIwH$giSZYZuxiRWbE1il^$Eqwwoj7xA#S');
-
